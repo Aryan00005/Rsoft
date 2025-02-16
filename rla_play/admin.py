5c391440e73812bacc48fb7d4bf34adc85441446
Aryan Bharakhada <146478092+Aryan00005@users.noreply.github.com>
2025-02-16 22:40:43 +0530

    changed typo

--- rla_play/admin.py
+++ rla_play/admin.py
@@ -2,6 +2,6 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-admin.site.site_header = _("RLA Play Admin")
-admin.site.site_title = _("RLA Play Admin Portal")
-admin.site.index_title = _("Welcome to RLA Play Admin")
+admin.site.site_header = _("RLA Pay Admin")
+admin.site.site_title = _("RLA Pay Admin Portal")
+admin.site.index_title = _("Welcome to RLA Pay Admin")
